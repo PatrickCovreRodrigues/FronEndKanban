606da149f5df79f06f2880129540d7baabb89c6e
PatrickCovreRodrigues <patrickcovredev@gmail.com>
2024-12-27 10:02:28 -0300

    add procol http put in projects

--- FastFront/src/components/CompponentListProject.vue
+++ FastFront/src/components/CompponentListProject.vue
@@ -11,6 +11,7 @@
       <template v-slot:top>
         <v-toolbar flat>
           <v-toolbar-title>Projetos</v-toolbar-title>
+          <button  @click="openPostProjectDialog" class="v-btn-creat">Criar Tarefa</button>
         </v-toolbar>
       </template>
 
@@ -19,6 +20,32 @@
         <v-icon @click="fetchDeleteProject(item.id)" class="red--text">mdi-delete</v-icon>
       </template>
     </v-data-table>
+
+     <v-dialog v-model="dialog" max-width="500px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">Editar Projeto</span>
+        </v-card-title>
+        <v-card-text>
+          <v-form ref="form">
+            <v-text-field v-model="editedItem.name" label="Nome"></v-text-field>
+            <v-text-field v-model="editedItem.description_project" label="Descrição"></v-text-field>
+            <v-select
+              v-model="editedItem.customer_id"
+              :items="customers"
+              item-title="name"
+              item-value="id"
+              label="Cliente"
+            ></v-select>
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="closeDialog">Cancelar</v-btn>
+          <v-btn color="blue darken-1" text @click="updateProject">Salvar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-card>
 </template>
 
@@ -27,8 +54,10 @@ import axios from "axios";
 
 export default {
   data: () => ({
-    items: [], 
+    items: [],
+    customers: [], 
     url: "http://127.0.0.1:8000/projects/", 
+    customersUrl: "http://127.0.0.1:8000/customers/",
     headers: [
       { title: "ID", value: "id" },
       { title: "Nome", value: "name" },
@@ -37,9 +66,18 @@ export default {
       { title: "Criado em", value: "created_at" },
       { title: "Ações", value: "actions", sortable: false },
     ],
+    dialog: false,
+    editedItem: {
+      id: null,
+      name: "",
+      description_project: "",
+      customer_id: null,
+    },
   }),
-  created() {
-    this.fetchGetProjects();
+  async created() {
+    await this.fetchGetProjects();
+    await this.fetchDeleteProject();
+    await this.fetchCustomers();
   },
   methods: {
     async fetchGetProjects() {
@@ -58,8 +96,34 @@ export default {
         console.error("Erro ao excluir projeto:", error);
       }
     },
+    async fetchCustomers() {
+      try {
+        const response = await axios.get(this.customersUrl);
+        this.customers = response.data;
+      } catch (error) {
+        console.error('Error fetching customers:', error);
+      }
+    },
     editProject(item) {
-      console.log("Editar projeto:", item);
+      this.editedItem = { ...item };
+      this.dialog = true;
+    },
+    closeDialog(){
+      this.dialog = false;
+    },
+    async updateProject() {
+      try {
+        const response = await axios.put(`${this.url}${this.editedItem.id}/`, this.editedItem);
+        const index = this.items.findIndex(item => item.id === this.editedItem.id);
+        if (index !== -1) {
+          this.items.splice(index, 1, response.data);
+        }
+        this.dialog = false;
+        alert("Projeto atualizado com sucesso!");
+      } catch (error) {
+        console.error("Erro ao atualizar projeto:", error);
+        alert("Erro ao atualizar projeto!");
+      }
     },
     formatDate(date) {
       if (!date) return "";
@@ -83,5 +147,14 @@ export default {
 }
 .v-data-table {
   margin-top: 16px;
+}
+.v-btn-creat {
+  margin: 20px;
+  border: 1px solid #3f51b5;
+  background-color: #3f51b5;
+  color: white;
+  border-radius: 4px;
+  width: 100px;
+  height: 40px;
 }
 </style>
